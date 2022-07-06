30f91bfacae3ffae4d635832ad9d13eb19b12f3c
Ajanitone <ajanitone@freenet.de>
2022-07-06 13:59:17 +0200

    initial commit

--- solution.js
+++ solution.js
@@ -1,1 +1,99 @@
 // write your code in this file
+
+const tickettotal = 6450;
+const oneTicket = 15;
+const ticketsales = tickettotal / oneTicket;
+console.log("Q1", ticketsales + " tickets sold");
+
+console.log("------------------------------");
+
+const income = 500;
+const yearIncome = income * 4 * 12;
+console.log("Q2: Sylvia makes", yearIncome + " yearly");
+
+const full = 30;
+const part = 17;
+const percentage = (17 / 30) * 100;
+
+console.log("------------------------------");
+
+console.log("Q3:", percentage + "%");
+
+console.log("------------------------------");
+
+const square = 4.75;
+const periMeter = 4.75 * 4;
+
+console.log("Q4:The perimeter of a square is ", periMeter);
+
+console.log("------------------------------");
+
+const triangle = 5;
+const triangle1 = 6;
+const periMeter1 = triangle * 2 + 1 + triangle1 + 1;
+
+console.log("Q5: The perimeter of a triangle is ", periMeter1);
+
+console.log("------------------------------");
+
+const square1 = 5;
+const squareArea = square1 * 5;
+
+console.log("Q6:The area of a square is ", squareArea);
+
+console.log("------------------------------");
+
+const rightTriangle = 4 * 5;
+const triArea = 0.5 * rightTriangle;
+
+console.log("Q7: The area of the triangle is", triArea);
+
+console.log("------------------------------");
+
+const cube = 9;
+const cubevolume = 9 * 3;
+
+console.log("Q8: The volume of the cube is ", cubevolume);
+
+const bill = 22.35;
+const billpercent = (22.35 / 100) * 10;
+const billTotal = bill + billpercent;
+
+console.log("Q9a: 22.35 plus 10% is equal to ", billTotal);
+
+console.log("------------------------------");
+
+const bill1 = 26.67;
+const billpercent1 = (26.67 / 100) * 15;
+const billTotal1 = bill1 + billpercent1;
+
+console.log("Q9b: 26.67 plus 15% is equal to ", billTotal1);
+
+const bill2 = 35.92;
+const billpercent2 = (35.92 / 100) * 20;
+const billTotal2 = bill2 + billpercent1;
+console.log("Q9c: 35.92 plus 20% is equal to ", billTotal2);
+
+console.log("------------------------------");
+
+const workHours = 63;
+const averageHours = workHours / 11;
+console.log("Q10: The average hours that Noemy worked is", averageHours);
+
+console.log("------------------------------");
+
+const totalScore = 420;
+const averageScore = totalScore / 5;
+const averageScore1 = averageScore / 6 + 1;
+
+console.log("Q11: Score in the sixth test is", averageScore1 * 6);
+
+console.log("------------------------------");
+
+const nine = 80;
+const eight = 78;
+const x = (eight / 2) * 2;
+const z = nine * 2;
+const s = z - x;
+
+console.log(`12:The score in the sixth text is ${s}`);
